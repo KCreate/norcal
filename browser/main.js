cbd1b93e486c86b6288e9babaf666eab56d76e6b
James Halliday <mail@substack.net>
2015-01-18 13:41:21 -0800

    month left/right

--- browser/main.js
+++ browser/main.js
@@ -38,9 +38,18 @@ var mleft = document.querySelector('#controls .month [name="left"]');
 mleft.addEventListener('click', function (ev) {
     var ix = months.indexOf(ucfirst(month.value));
     if (ix < 0) return;
-    if (ix === 0) year.value = Number(year.value - 1);
+    if (ix === 0) year.value = Number(year.value) - 1;
     cmonth.set(months[(ix + 11) % 12]);
     showMonth(year.value, (ix + 11) % 12);
+});
+
+var mright = document.querySelector('#controls .month [name="right"]');
+mright.addEventListener('click', function (ev) {
+    var ix = months.indexOf(ucfirst(month.value));
+    if (ix < 0) return;
+    if (ix === 11) year.value = Number(year.value) + 1;
+    cmonth.set(months[(ix + 1) % 12]);
+    showMonth(year.value, (ix + 1) % 12);
 });
 
 var year = document.querySelector('#controls [name="year"]');
