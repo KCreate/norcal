bfd0c048b331de7656da7956e6290c7eb7c7067f
James Halliday <mail@substack.net>
2015-01-18 00:31:26 -0800

    all the days in the month

--- server.js
+++ server.js
@@ -1,25 +1,17 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
-
 var hyperstream = require('hyperstream');
-var template = require('html-template');
 
 var ecstatic = require('ecstatic');
 var st = ecstatic(__dirname + '/static');
 
+var month = require('./lib/month.js');
+
 var server = http.createServer(function (req, res) {
     if (req.url === '/') {
-        var html = template();
-        var days = html.template('day');
-        days.write({ '.date': 1 });
-        days.write({ '.date': 2 });
-        days.write({ '.date': 3 });
-        days.write({ '.date': 4 });
-        days.end();
-        
         read('index.html').pipe(hyperstream({
-            '#calendar': read('calendar.html').pipe(html)
+            '#calendar': month(new Date)
         })).pipe(res);
     }
     else st(req, res);
